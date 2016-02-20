2e294cee312fd03d730607bfa815fc24102a27c5
Daniel Ashton <jdashton@ashtonfam.org>
2016-02-20 17:21:35 -0500

    Change board to 64x64, disable down and left

--- js/keyboard_input_manager.js
+++ js/keyboard_input_manager.js
@@ -37,16 +37,16 @@ KeyboardInputManager.prototype.listen = function () {
   var map = {
     38: 0, // Up
     39: 1, // Right
-    40: 2, // Down
-    37: 3, // Left
+    //40: 2, // Down
+    //37: 3, // Left
     75: 0, // Vim up
     76: 1, // Vim right
-    74: 2, // Vim down
-    72: 3, // Vim left
+    //74: 2, // Vim down
+    //72: 3, // Vim left
     87: 0, // W
     68: 1, // D
-    83: 2, // S
-    65: 3  // A
+    //83: 2, // S
+    //65: 3  // A
   };
 
   // Respond to direction keys
